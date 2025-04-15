0205587ad8ca29267756bb71f2e20b14d2d82755
Daniela-MV <danmv.0991@gmail.com>
2025-04-15 23:00:32 +0000

    Creación de cards y map

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -10,14 +10,19 @@ import CardImage from "./cards";
 
 //create your first component
 const Home = () => {
+	let cards = [
+		{título: "La nueva tecnología", descripcion: "Ya no estamos tan lejos de poder tener autos voladores"}
+		{título: "Crisis mundial", descripcion: "Ahora cada vez más votantes que estaban a favor de Trump se encuentran arrepentidos"}
+		{título: "Los milenials no pueden adquirir una casa", descripcion: "Cada vez son más los jóvenes a los que no les queda otra opción más que vivir en casa de sus padres debido a la inflación"}
+		
+	]
 	return (
 		<div className="text-center">
             <Navbar/>
 			<Jumbutron/>
-			<CardImage/>
-			<CardImage/>
-			<CardImage/>
-			<CardImage/>
+			<div>
+				<cards
+			</div>
 
 		</div>
 	);
