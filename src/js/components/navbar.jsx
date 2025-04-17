1dbeaeff4e5ea628b1decbaa0bf426e402f59221
Daniela-MV <danmv.0991@gmail.com>
2025-04-17 01:31:10 +0000

    Ahora sí es el final pro

--- src/js/components/navbar.jsx
+++ src/js/components/navbar.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Navbar = () => {
 return(
     <nav className="navbar navbar-expand-lg bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Start Bootstrap</a>
+  <div className="container-fluid bg-secondary">
+    <a className="navbar-brand text-light fs-2" href="#">Start Bootstrap</a>
     
     <div className= "position-absolute top-0 end-0">
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,10 +12,10 @@ return(
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div className="navbar-nav">
-        <a className="nav-link active" aria-current="page" href="#">Home</a>
-        <a className="nav-link" href="#">About</a>
-        <a className="nav-link" href="#">Service</a>
-        <a className="nav-link disabled">Contact</a>
+        <a className="nav-link active text-light fs-4" aria-current="page" href="#">Home</a>
+        <a className="nav-link text-light fs-4" href="#">About</a>
+        <a className="nav-link text-light fs-4" href="#">Service</a>
+        <a className="nav-link disabled text-light fs-4">Contact</a>
       </div>
     </div>
     </div>
