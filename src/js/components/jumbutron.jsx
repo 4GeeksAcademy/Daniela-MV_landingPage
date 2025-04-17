71d6ee23e010ececcfbfbeca74020ef23f7e62a4
Daniela-MV <danmv.0991@gmail.com>
2025-04-17 00:01:13 +0000

    finalizado ahora sí

--- src/js/components/jumbutron.jsx
+++ src/js/components/jumbutron.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Jumbutron = () => {
     return (
-        <div className="card">
-            <div className="card-body text-start">
+        <div className="card mb-4">
+            <div className="card-body text-start bg-secondary-subtle">
                 <h5 className="card-title fs-1">Grandes Noticias!</h5>
                 <p className="card-text">Estas son las últimas noticias, lo que está aconteciendo en el mundo actual.</p>
                 <a href="#" className="btn btn-primary">Haz click aquí</a>
